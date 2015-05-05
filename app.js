aeeccc4f57ec72da407816a34b7b7d8a2434dba3
vishal <vishal41190@gmail.com>
2015-05-04 21:23:55 -0700

    Perform JSHint

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ app.controller("chat",function($scope){
     $scope.commentList=["This is the first commnet","Here's the second comment","And this is one More","Here is another one"]; 
 
     $scope.addComment = function(){
-        if($scope.chatInput== null){
+        if($scope.chatInput===null){
             window.alert("Comment can't be empty");
         }else{
             $scope.commentList.push($scope.chatInput);
+            $scope.chatInput='';
         }
-    }
+    };
 });
-
 
